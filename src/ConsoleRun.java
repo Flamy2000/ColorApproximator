799e1db0bc75f378bf8d98e47280839068be6d32
Flamy2000 <82188770+Flamy2000@users.noreply.github.com>
2023-03-05 18:15:26 -0600

    renamed "main" file; added new image

---
+++ src/ConsoleRun.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ConsoleRun {
+    public static void main(String[] args) throws FileNotFoundException {
+        Color[] palette = ColorApproximation.readColorFile(new File(System.getProperty("user.dir") + "\\Colors\\Crayola100SuperTipMarkers.txt"));
+        ColorApproximation ca = new ColorApproximation(System.getProperty("user.dir") + "\\SampleImages\\windows.jpg", palette);
+        ca.run(); // run without threading
+        ca.showOutput();
+
+    }
+}
